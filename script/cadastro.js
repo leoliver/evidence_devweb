46b4fc6297ebcbb1664e887f5719d21e90650644
Leonardo Oliveira <77712075+leoliver@users.noreply.github.com>
2024-09-12 15:38:45 -0300

    Add files via upload

---
+++ script/cadastro.js
@@ -0,0 +1,43 @@
+
+const url = "https://go-wash-api.onrender.com/api/user"; 
+async function cadastro(){
+    let nome = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let senha = document.getElementById('password').value;
+    let cpf_cnpj = document.getElementById('cpf_cnpj').value;
+    let nascimento = document.getElementById('birthday').value;
+    let termo = document.getElementById('terms').checked;
+    form = {};
+
+    if (!termo || !nome || !email || !senha || !cpf_cnpj || !nascimento){
+        alert("Preencha os campos corretamente");
+        return;
+    } else {
+        termo = 1;
+        form = {
+            "name": nome,
+            "email": email,
+            "user_type": 1,
+            "password": senha,
+            "cpf_cnpj": cpf_cnpj,
+            "terms": termo,
+            "birthday": nascimento
+        };
+    }
+
+    let api = await fetch(url,{
+        method:"POST",
+        body:JSON.stringify(form),
+        headers:{
+            'Content-Type':'application/json'
+        }
+    });
+
+    if(api.ok){
+        let resposta = await api.json();
+        console.log(resposta);
+        return;
+    }
+    let respostaErro = await api.json();
+        console.log(respostaErro.data.errors.cpf_cnpj);
+}
