c214d3b761bc27ed531f4549da6dc59fad0d2a78
leoliver <leosdoliver@gmail.com>
2024-10-22 14:39:16 -0300

    Fianlização listagem de endereços

--- script/home.js
+++ script/home.js
@@ -19,31 +19,19 @@ async function listarEnderecos() {
 
         lista_enderecos = document.getElementById("enderecos_listados")
 
-        let teste = [
-            {title: "Casa", cep: "05640001", address: "Rua xyz", number: 123},
-            {title: "Casa", cep: "05640001", address: "Rua xyz", number: 123}
-        ]
-
-        // teste.forEach(endereco => {
-        //     let linha = document.createElement("tr") 
-        //     linha.innerHTML = `
-        //         <td>${endereco.title}</td>
-        //         <td>${endereco.cep}</td>
-        //         <td>${endereco.address}</td>
-        //         <td>${endereco.number}</td>
-        //     `
-        //     lista_enderecos.appendChild(linha)
-        // });
-        
-
         resposta.data.forEach(endereco => {
-            // console.log(endereco)
             let linha = document.createElement("tr") 
             linha.innerHTML = `
-                <td>${endereco.title}</td>
-                <td>${endereco.cep}</td>
-                <td>${endereco.address}</td>
-                <td>${endereco.number}</td>
+                <tr>
+                    <td>${endereco.id}</td>
+                    <td>${endereco.title}</td>
+                    <td>${endereco.cep}</td>
+                    <td>${endereco.address}</td>
+                    <td>${endereco.number}</td>
+                    <td>${!endereco.complement ? "N/A" : endereco.complement}</td>
+                    <td><button type="button" class="enderecobutao" onclick="EditarEndereco()">Editar</button></td>
+                    <td><button type="button" class="enderecobutao" onclick="DeletarEndereco()">Deletar</button></td>
+                </tr>
             `
             lista_enderecos.appendChild(linha)
         });
